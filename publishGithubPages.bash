500610a547e45efbca6a2a9ccaab3d758540744c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-05-25 15:11:45 -0700

    cleanup

--- publishGithubPages.bash
+++ publishGithubPages.bash
@@ -9,9 +9,9 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 
 touch .nojekyll
+git add .nojekyll
 git checkout master SpatialDatabase.Rproj
 git checkout master .gitignore
-git checkout master index.html
 git checkout master Docs
 git status
 git commit -m 'Updating docs from "master"'
