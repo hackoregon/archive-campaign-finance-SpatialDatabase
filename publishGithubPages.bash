487872f23c40cb4e71141821c69e42288dacda96
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-05-25 14:56:57 -0700

    adding index.html

--- publishGithubPages.bash
+++ publishGithubPages.bash
@@ -8,8 +8,10 @@
 # MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 
+touch .nojekyll
 git checkout master SpatialDatabase.Rproj
 git checkout master .gitignore
+git checkout master index.html
 git checkout master Docs
 git status
 git commit -m 'Updating docs from "master"'
