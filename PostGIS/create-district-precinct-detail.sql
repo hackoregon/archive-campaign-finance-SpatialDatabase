e8702d72a1e8980072dd9f39e6b5918fa550692d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-05-25 20:40:04 -0700

    create district precinct detail table

---
+++ PostGIS/create-district-precinct-detail.sql
@@ -0,0 +1,21 @@
+﻿-- Table: district_precinct_detail
+
+-- DROP TABLE district_precinct_detail;
+
+CREATE TABLE district_precinct_detail
+(
+  county text,
+  precinct text,
+  precint_name text,
+  split text,
+  district_code text,
+  district_name text,
+  district_type text
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE district_precinct_detail
+  OWNER TO znmeb;
+
+COPY district_precinct_detail FROM '/gisdata/district-precinct-detail.txt' WITH ( FORMAT text );
