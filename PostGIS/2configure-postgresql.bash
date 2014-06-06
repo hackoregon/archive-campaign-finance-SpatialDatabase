6c201faf669ca516c015271f2f20064c8c2c5cc3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-05 17:25:19 -0700

    refactoring in progress

--- PostGIS/2configure-postgresql.bash
+++ PostGIS/2configure-postgresql.bash
@@ -9,15 +9,19 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
+export HERE=`pwd`
 sudo postgresql-setup initdb # fails harmlessly if data directory isn't empty
 sudo systemctl enable postgresql # start the server on reboot
 sudo systemctl start postgresql # start the server now
 
 # password protect the PostgreSQL superuser, 'postgres'
-echo "Create a password for 'postgres', the PostgreSQL superuser"
+echo "Create a PostgreSQL password for 'postgres', the PostgreSQL superuser"
 sudo su - postgres -c "psql -c '\password postgres'"
 
 # install the extensions - will ERROR harmlessly if they're already there
+sudo su - postgres -c \
+  "psql < ${HERE}/create-extensions.psql"
+exit
 sudo su - postgres -c "psql -c 'CREATE EXTENSION adminpack;'"
 sudo su - postgres -c \
   "psql -c 'CREATE EXTENSION plpgsql WITH SCHEMA pg_catalog;'"
