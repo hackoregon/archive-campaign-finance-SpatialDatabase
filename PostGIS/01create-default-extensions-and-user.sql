f9f7ee07ceeb58cbd8115454d80311101bee4a78
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-06 15:19:44 -0700

    put the geocoder stuff in the right database

--- PostGIS/01create-default-extensions-and-user.sql
+++ PostGIS/01create-default-extensions-and-user.sql
@@ -1,14 +1,25 @@
-﻿CREATE EXTENSION IF NOT EXISTS adminpack;
+CREATE EXTENSION IF NOT EXISTS adminpack;
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 DROP DATABASE IF EXISTS znmeb;
 DROP DATABASE IF EXISTS voter_reg;
 DROP DATABASE IF EXISTS geocoder;
 DROP ROLE IF EXISTS znmeb;
+
 CREATE ROLE znmeb 
-  WITH LOGIN NOSUPERUSER INHERIT CREATEDB NOCREATEROLE NOREPLICATION; 
+  WITH LOGIN NOSUPERUSER INHERIT CREATEDB NOCREATEROLE NOREPLICATION;
+\password znmeb
+
 CREATE DATABASE znmeb
   WITH OWNER znmeb;
+\connect znmeb
+\i create-postgis-extensions.sql
+
 CREATE DATABASE voter_reg
   WITH OWNER znmeb;
+\connect voter_reg
+\i create-postgis-extensions.sql
+
 CREATE DATABASE geocoder
   WITH OWNER postgres
+\connect geocoder
+\i create-geocoder-extensions.sql
