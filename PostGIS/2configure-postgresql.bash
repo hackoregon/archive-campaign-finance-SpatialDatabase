6ac5a0f0bd0821a8f3f6aefd420ccf09c6f02c01
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-05 18:50:33 -0700

    refactored - now use MD5 authentication for Windows compatibility

--- PostGIS/2configure-postgresql.bash
+++ PostGIS/2configure-postgresql.bash
@@ -12,19 +12,12 @@
 sudo postgresql-setup initdb # fails harmlessly if data directory isn't empty
 sudo systemctl enable postgresql # start the server on reboot
 sudo systemctl start postgresql # start the server now
+sudo patch -N -b /var/lib/pgsql/data/pg_hba.conf pg_hba.conf.patch
+sudo systemctl restart postgresql # restart the server
 
 # password protect the PostgreSQL superuser, 'postgres'
 echo "Create a PostgreSQL password for 'postgres', the PostgreSQL superuser"
-#sudo su - postgres -c "psql -c '\password postgres'"
+sudo su - postgres -c "psql -c '\password postgres'"
 
-# install the extensions - will ERROR harmlessly if they're already there
-sudo mkdir -p /gisdata
-sudo chown -R ${USER}:${USER} /gisdata
-cp create-extensions.psql /gisdata
-sudo chown -R postgres:postgres /gisdata
-sudo su - postgres -c \
-  "psql -f /gisdata/create-extensions.psql"
-exit
-sudo su - postgres -c "psql -c 'CREATE EXTENSION adminpack;'"
-sudo su - postgres -c \
-  "psql -c 'CREATE EXTENSION plpgsql WITH SCHEMA pg_catalog;'"
+# install default extensions - will ERROR harmlessly if they're already there
+psql -d postgres -U postgres -W -f create-default-extensions.psql
