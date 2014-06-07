cf02fbcdc249d3e5f5095aa558dd9801166f406e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-06 19:59:54 -0700

    make views after data loaded

--- PostGIS/DistrictPrecinctDetail.sql
+++ PostGIS/DistrictPrecinctDetail.sql
@@ -10,3 +10,24 @@ CREATE TABLE district_precinct_detail (
 );
 ALTER TABLE district_precinct_detail OWNER TO znmeb;
 \copy district_precinct_detail from '/gisdata/DistrictPrecinctDetail.txt';
+
+CREATE OR REPLACE VIEW cleaned_district_precinct_detail AS
+  SELECT 
+    district_precinct_detail.county, 
+    district_precinct_detail.precinct, 
+    district_precinct_detail.precinct_name, 
+    district_precinct_detail.split, 
+    district_precinct_detail.district_code, 
+    district_precinct_detail.district_name, 
+    district_precinct_detail.district_type
+  FROM 
+    public.district_precinct_detail
+  WHERE 
+    district_precinct_detail.district_type != 'FEDERAL STATEWIDE' AND 
+    district_precinct_detail.district_type != 'STATEWIDE PARTISAN' AND 
+    district_precinct_detail.district_type != 'STATEWIDE NONPARTISAN'
+  ORDER BY
+    district_precinct_detail.county ASC, 
+    district_precinct_detail.precinct ASC, 
+    district_precinct_detail.split ASC, 
+    district_precinct_detail.district_code ASC;
--- PostGIS/RegisteredVoters.sql
+++ PostGIS/RegisteredVoters.sql
@@ -43,3 +43,39 @@ CREATE TABLE registered_voters (
 );
 ALTER TABLE registered_voters OWNER TO znmeb;
 \copy registered_voters from '/gisdata/RegisteredVoters.txt';
+
+DROP VIEW IF EXISTS public.duplicate_voter_ids CASCADE;
+CREATE OR REPLACE VIEW public.duplicate_voter_ids AS 
+ SELECT registered_voters.voter_id,
+    count(registered_voters.voter_id) AS id_count
+   FROM registered_voters
+  GROUP BY registered_voters.voter_id
+ HAVING count(registered_voters.voter_id) > 1;
+
+ALTER TABLE public.duplicate_voter_ids
+  OWNER TO znmeb;
+
+DROP VIEW IF EXISTS public.show_duplicate_voter_ids CASCADE;
+CREATE OR REPLACE VIEW public.show_duplicate_voter_ids AS 
+ SELECT registered_voters.voter_id,
+    registered_voters.status,
+    registered_voters.party_code,
+    registered_voters.first_name,
+    registered_voters.last_name,
+    registered_voters.res_address_1,
+    registered_voters.eff_address_1,
+    registered_voters.city,
+    registered_voters.eff_city,
+    registered_voters.state,
+    registered_voters.eff_state,
+    registered_voters.zip_code,
+    registered_voters.eff_zip_code,
+    registered_voters.county,
+    registered_voters.precinct,
+    registered_voters.split
+   FROM duplicate_voter_ids,
+    registered_voters
+  WHERE registered_voters.voter_id = duplicate_voter_ids.voter_id;
+
+ALTER TABLE public.show_duplicate_voter_ids
+  OWNER TO znmeb;
