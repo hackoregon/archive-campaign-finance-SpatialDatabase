32a6d8ab9feb65bd5563bb0740b437510c21560e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-07 19:50:51 -0700

    more databases to drop for USER

--- PostGIS/01create-default-extensions-and-user.sql
+++ PostGIS/01create-default-extensions-and-user.sql
@@ -3,6 +3,12 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 DROP DATABASE IF EXISTS znmeb;
 DROP DATABASE IF EXISTS voter_reg;
 DROP DATABASE IF EXISTS geocoder;
+DROP DATABASE IF EXISTS congress_districts;
+DROP DATABASE IF EXISTS elementary_school_districts;
+DROP DATABASE IF EXISTS secondary_school_districts;
+DROP DATABASE IF EXISTS unified_school_districts;
+DROP DATABASE IF EXISTS state_legislature_lower_districts;
+DROP DATABASE IF EXISTS state_legislature_upper_districts;
 DROP ROLE IF EXISTS znmeb;
 
 CREATE ROLE znmeb 
