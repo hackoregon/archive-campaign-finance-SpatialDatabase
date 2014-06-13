3db51df68754f019db570dcc1f0515b77487497e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-13 13:27:38 -0700

    using batch geocoder from PostGIS in Action

---
+++ PostGIS/VoterRegistration/batch-geocode.sql
@@ -0,0 +1,13 @@
+﻿UPDATE normalized_addresses
+SET
+(geomout, rating, lon, lat) = (
+  (g).geomout,
+  COALESCE((g).rating, -1),
+  ST_X((g).geomout),
+  ST_Y((g).geomout)
+)
+FROM (SELECT * FROM normalized_addresses
+WHERE rating IS NULL LIMIT 10000) AS a
+LEFT JOIN LATERAL geocode(a.normalized_address, 1) AS g
+ON ((g).rating < 22)
+WHERE a.addid = normalized_addresses.addid;
