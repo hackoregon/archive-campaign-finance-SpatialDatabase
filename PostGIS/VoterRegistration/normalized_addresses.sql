79db3671d6736e6cd6b609a8e5c4daa5e82882d4
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-13 10:32:08 -0700

    added the primary key

--- PostGIS/VoterRegistration/normalized_addresses.sql
+++ PostGIS/VoterRegistration/normalized_addresses.sql
@@ -12,3 +12,8 @@ WITH (
 ALTER TABLE public.normalized_addresses
   OWNER TO znmeb;
 \copy normalized_addresses FROM '/gisdata/geocoder-data/normalized_addresses.csv' WITH (FORMAT CSV);
+ALTER TABLE public.normalized_addresses ADD COLUMN geomout geometry;
+ALTER TABLE public.normalized_addresses ADD COLUMN rating integer;
+ALTER TABLE public.normalized_addresses ADD COLUMN lon double precision;
+ALTER TABLE public.normalized_addresses ADD COLUMN lat double precision;
+ALTER TABLE public.normalized_addresses ADD COLUMN addid serial PRIMARY KEY;
