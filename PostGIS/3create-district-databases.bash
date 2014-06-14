0d40a158bbf3c50f811133a7b98ecd6a811aa45d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-13 19:38:55 -0700

    consolidating all the TIGER stuff into a single script

--- PostGIS/3create-district-databases.bash
+++ PostGIS/3create-district-databases.bash
@@ -9,9 +9,23 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# run the GeoJSON creator first to snag all the data
-../GeoJSON/make-tiger-geojson.bash
+# TIGER/Line® documentation:
+#    http://www.census.gov/geo/education/howtos.html
+# Folder names:
+#   ftp://ftp2.census.gov/geo/tiger/TIGER2013/2013-FolderNames-Defined.pdf
+# Dependencies: GDAL - Geospatial Data Abstraction Library 1.11.0 or later
+#               wget
+#               unzip
+#               zip
+#
+# This should work on Ubuntu; I'll test in the VM
+# This might work on MacOS X if you have all the dependencies; someone test ;-)
 
+sudo mkdir -p /gisdata
+sudo mkdir -p /gisdata/shapefiles
+sudo mkdir -p /gisdata/GeoJSON
+sudo mkdir -p /gisdata/GeoJSONzip
+sudo chown -R ${USER}:${USER} /gisdata
 cp create-postgis-extensions.sql /gisdata
 cp dump-database.bash /gisdata
 chmod +x /gisdata/*.bash
@@ -41,7 +55,28 @@ popd
 # ZIP Code Tabulation Areas: ftp://ftp2.census.gov/geo/tiger/TIGER2013/ZCTA5/
 for i in STATE COUNTY CD ZCTA5
 do
-  j=`echo ${i} | tr [:upper:] [:lower:]` # database names need to be lowercase
+  wget \
+    --quiet \
+    --no-parent \
+    --relative \
+    --recursive \
+    --level=1 \
+    --accept=zip \
+    --reject=html \
+    --mirror \
+    "ftp://ftp2.census.gov/geo/tiger/TIGER2013/${i}/tl_2013_us*zip" 
+  mkdir -p shapefiles/${i}
+  unzip -n -u -d shapefiles/${i} \
+    "ftp2.census.gov/geo/tiger/TIGER2013/${i}/tl_2013_us*zip" 
+  SOURCE=`find shapefiles/${i} -name '*.shp'`
+  DEST=`echo ${SOURCE}|sed 's/shp/geojson/'|sed 's/shapefiles/GeoJSON/'`
+  mkdir -p GeoJSON/${i}
+
+  # this step will throw errors if the GeoJSON is already done, saving time
+  ogr2ogr -f GeoJSON ${DEST} ${SOURCE}
+  zip -9ur GeoJSONzip/${i}.zip ${DEST}
+
+  j=`echo ${i} | tr [:upper:] [:lower:]` # database names should be lowercase
   pushd shapefiles/${i}
   psql -U ${USER} -d ${USER} -c "DROP DATABASE IF EXISTS ${j};"
   psql -U ${USER} -d ${USER} -c "CREATE DATABASE ${j} WITH OWNER ${USER};"
@@ -58,14 +93,40 @@ do
 done
 
 # state of Oregon
+# State Senators: ftp://ftp2.census.gov/geo/tiger/TIGER2013/SLDU/
+# State Representatives: ftp://ftp2.census.gov/geo/tiger/TIGER2013/SLDL/
 # Elementary School Districts: ftp://ftp2.census.gov/geo/tiger/TIGER2013/ELSD/
 # Secondary School Districts: ftp://ftp2.census.gov/geo/tiger/TIGER2013/SCSD/
-# State Representatives: ftp://ftp2.census.gov/geo/tiger/TIGER2013/SLDL/
-# State Senators: ftp://ftp2.census.gov/geo/tiger/TIGER2013/SLDU/
 # Unified School Districts: ftp://ftp2.census.gov/geo/tiger/TIGER2013/UNSD/
-for i in ELSD SCSD SLDL SLDU UNSD
+# Block Groups: ftp://ftp2.census.gov/geo/tiger/TIGER2013/BG/
+# County Subdivisions: ftp://ftp2.census.gov/geo/tiger/TIGER2013/COUSUB/
+# Places: ftp://ftp2.census.gov/geo/tiger/TIGER2013/PLACE/
+# Tabulation Blocks: ftp://ftp2.census.gov/geo/tiger/TIGER2013/TABBLOCK/
+# Tracts: ftp://ftp2.census.gov/geo/tiger/TIGER2013/TRACT/
+for i in SLDU SLDL ELSD SCSD UNSD BG COUSUB PLACE TABBLOCK TRACT
 do
-  j=`echo ${i} | tr [:upper:] [:lower:]` # database names need to be lowercase
+  wget \
+    --quiet \
+    --no-parent \
+    --relative \
+    --recursive \
+    --level=1 \
+    --accept=zip \
+    --reject=html \
+    --mirror \
+    "ftp://ftp2.census.gov/geo/tiger/TIGER2013/${i}/tl_2013_41*zip" 
+  mkdir -p shapefiles/${i}
+  unzip -n -u -d shapefiles/${i} \
+    "ftp2.census.gov/geo/tiger/TIGER2013/${i}/tl_2013_41*zip" 
+  SOURCE=`find shapefiles/${i} -name '*.shp'`
+  DEST=`echo ${SOURCE}|sed 's/shp/geojson/'|sed 's/shapefiles/GeoJSON/'`
+  mkdir -p GeoJSON/${i}
+
+  # this step will throw errors if the GeoJSON is already done, saving time
+  ogr2ogr -f GeoJSON ${DEST} ${SOURCE}
+  zip -9ur GeoJSONzip/${i}.zip ${DEST}
+
+  j=`echo ${i} | tr [:upper:] [:lower:]` # database names should be lowercase
   pushd shapefiles/${i}
   psql -U ${USER} -d ${USER} -c "DROP DATABASE IF EXISTS ${j};"
   psql -U ${USER} -d ${USER} -c "CREATE DATABASE ${j} WITH OWNER ${USER};"
@@ -80,3 +141,40 @@ do
   /gisdata/dump-database.bash ${j}
   popd
 done
+
+# Oregon counties - geocoder uses them so we prefetch the shapefiles
+# Addresses: ftp://ftp2.census.gov/geo/tiger/TIGER2013/ADDR/
+# Edges: ftp://ftp2.census.gov/geo/tiger/TIGER2013/EDGES/
+# Faces: ftp://ftp2.census.gov/geo/tiger/TIGER2013/FACES/
+# Feature Names: ftp://ftp2.census.gov/geo/tiger/TIGER2013/FEATNAMES/
+for i in ADDR EDGES FACES FEATNAMES 
+do
+  wget \
+    --quiet \
+    --no-parent \
+    --relative \
+    --recursive \
+    --level=1 \
+    --accept=zip \
+    --reject=html \
+    --mirror \
+    "ftp://ftp2.census.gov/geo/tiger/TIGER2013/${i}/tl_2013_41*zip" 
+done
+
+# dump the file sizes
+pushd ftp2.census.gov/geo/tiger/TIGER2013
+echo "Compressed shapefile sizes"
+du -sh *
+popd
+pushd shapefiles
+echo "Uncompressed shapefile sizes"
+du -sh *
+popd
+pushd GeoJSON
+echo "Uncompressed GeoJSON sizes"
+du -sh *
+popd
+pushd GeoJSONzip
+echo "Compressed GeoJSON sizes"
+du -sh *
+popd
