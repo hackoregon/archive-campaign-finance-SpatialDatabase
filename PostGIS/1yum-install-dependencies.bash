f73a400f6cf123ad7a939b98efaab3693c1f4a48
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-16 16:32:51 -0700

    full spatial complement from CompJournoStick

--- PostGIS/1yum-install-dependencies.bash
+++ PostGIS/1yum-install-dependencies.bash
@@ -16,22 +16,64 @@ sudo yum install -y \
   perl-Regexp-Assemble \
   subversion \
   patch \
+  gdal \
+  gdal-devel \
+  gdal-doc \
+  gdal-ruby \
+  gdal-perl \
+  gdal-python \
+  dans-gdal-scripts \
+  grass \
+  grass-devel \
+  proj \
+  proj-devel \
+  proj-epsg \
+  proj-nad \
+  geos \
+  geos-devel \
+  geos-python \
+  GMT \
+  GMT-coastlines \
+  GMT-doc \
+  GMT-coastlines-full \
+  GMT-coastlines-high \
+  GMT-common \
+  GMT-devel \
+  foxtrotgps \
+  gpscorrelate \
+  gpsdrive \
+  viking \
+  zyGrib \
+  josm \
+  carto \
+  mapnik \
+  mapnik-demo \
+  mapnik-utils \
+  marble \
+  merkaartor \
+  osm-gps-map \
+  osm-gps-map-devel \
+  osm-gps-map-gobject \
+  osm2pgsql \
+  python-phyghtmap \
+  pgRouting \
   postgis \
   postgis-docs \
   postgis-utils \
-  pgRouting \
-  pgadmin3 \
   qgis \
+  qgis-devel \
   qgis-grass \
-  qgis-mapserver \
   qgis-python \
-  josm \
-  merkaartor \
-  osm2pgsql \
-  osm-gps-map \
+  qlandkartegt \
   readosm \
+  readosm-devel \
   routino \
-  dans-gdal-scripts \
-  gdal-doc \
-  gdal-python
+  spatialite-gui \
+  spatialite-tools \
+  libspatialite-devel \
+  librasterlite-devel \
+  saga \
+  saga-devel \
+  mapserver \
+  qgis-mapserver
 sudo ./install-pagc-address-standardizer.bash
