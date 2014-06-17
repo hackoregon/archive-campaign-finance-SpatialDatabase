8a43bd40101b32fa7baef686eafb4f5ac77edf7c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-16 21:49:17 -0700

    added VACUUM

--- PostGIS/run-geocoder-scripts.bash
+++ PostGIS/run-geocoder-scripts.bash
@@ -9,11 +9,20 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# edit to store the 'postgres' database password
-vim national.bash oregon.bash
+# create database and generate the scripts
+cd /gisdata # just in case
+psql -f sql/create-geocoder-database.sql
 
-# now run the scripts and capture log files
-for i in national oregon
+pushd bash
+for i in 'national' 'oregon'
 do
+  sed -i 's;export PGBIN=/usr/pgsql-9.0/bin;export PGBIN=/usr/bin;' ${i}.bash
+  sed -i 's;--no-parent;--quiet --no-parent;' ${i}.bash
+  vim ${i}.bash
+  chmod +x ${i}.bash
   ./${i}.bash 2>&1 | grep -v ^INSERT | tee ${i}.log
 done
+popd
+
+psql -d geocoder -c "SELECT install_missing_indexes();"
+psql -d geocoder -c "VACUUM VERBOSE ANALYZE;"
