61d026e0766dc749b3590d6ced05d36adbcd3f50
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-21 20:20:02 -0700

    need -h on the grep

--- PostGIS/run-geocoder-scripts.bash
+++ PostGIS/run-geocoder-scripts.bash
@@ -14,7 +14,7 @@ cd /gisdata # just in case
 psql -d geocoder -f sql/make-scripts.sql
 
 # pre-fetch all the shapefiles!
-grep ^wget bash/national.bash bash/states.bash > bash/prefetch.bash
+grep -h ^wget bash/national.bash bash/states.bash > bash/prefetch.bash
 chmod +x bash/prefetch.bash
 bash/prefetch.bash
 
