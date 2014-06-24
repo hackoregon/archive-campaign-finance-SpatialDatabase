c243b146a2089972e9e984b3d85a54053807a3bd
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-23 21:04:59 -0700

    us_geocoder database

--- OrestarGeotagging/load-orestar.bash
+++ OrestarGeotagging/load-orestar.bash
@@ -9,23 +9,28 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# create an 'orestar' schema in the 'geocoder' database
+# create an 'orestar' schema in the 'us_geocoder' database
 for i in \
   "DROP SCHEMA IF EXISTS orestar CASCADE;" \
   "CREATE SCHEMA orestar AUTHORIZATION ${USER};"
 do
-  sudo su - postgres -c "psql -d geocoder -c '${i}'"
+  sudo su - postgres -c "psql -d us_geocoder -c '${i}'"
 done
 
 # restore the ORESTAR files - they go into 'public' schema by default
-sudo su - postgres -c "bzip2 -dc ${1} | psql -d geocoder"
+sudo su - postgres -c "bzip2 -dc ${1} | psql -d us_geocoder"
 
 # move the tables to the orestar schema
 for i in \
-  "ALTER TABLE IF EXISTS raw_committees OWNER TO ${USER};" \
-  "ALTER TABLE IF EXISTS raw_committees SET SCHEMA orestar;" \
-  "ALTER TABLE IF EXISTS raw_committee_transactions OWNER TO ${USER};" \
-  "ALTER TABLE IF EXISTS raw_committee_transactions SET SCHEMA orestar;"
+  "ALTER TABLE raw_committees OWNER TO ${USER};" \
+  "ALTER TABLE raw_committees SET SCHEMA orestar;" \
+  "ALTER TABLE raw_committee_transactions OWNER TO ${USER};" \
+  "ALTER TABLE raw_committee_transactions ADD COLUMN addy norm_addy;" \
+  "ALTER TABLE raw_committee_transactions ADD COLUMN geomout geometry;" \
+  "ALTER TABLE raw_committee_transactions ADD COLUMN rating integer;" \
+  "ALTER TABLE raw_committee_transactions ADD COLUMN lon double precision;" \
+  "ALTER TABLE raw_committee_transactions ADD COLUMN lat double precision;" \
+  "ALTER TABLE raw_committee_transactions SET SCHEMA orestar;"
 do
-  sudo su - postgres -c "psql -d geocoder -c '${i}'"
+  sudo su - postgres -c "psql -d us_geocoder -c '${i}'"
 done
