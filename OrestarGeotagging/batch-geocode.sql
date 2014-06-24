a70626d1907fe0fa8d9cd53dfe66a210f6db19cf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-06-23 20:57:49 -0700

    all fields except addr line 2 must be non-null

---
+++ OrestarGeotagging/batch-geocode.sql
@@ -0,0 +1,27 @@
+UPDATE orestar.raw_committee_transactions
+SET
+(addy, geomout, rating, lon, lat) = (
+  (g).addy,
+  (g).geomout,
+  COALESCE((g).rating, 9999),
+  ST_X((g).geomout),
+  ST_Y((g).geomout)
+)
+FROM (SELECT * FROM orestar.raw_committee_transactions
+WHERE rating IS NULL 
+AND addr_line1 IS NOT NULL
+AND city IS NOT NULL
+AND state IS NOT NULL
+AND zip IS NOT NULL
+LIMIT 2000) AS a
+LEFT JOIN LATERAL
+  geocode(concat_ws(' ', 
+    a.addr_line1, 
+    COALESCE(a.addr_line2, ' '),
+    a.city, 
+    a.state, 
+    a.zip
+  ), 1)
+AS g
+ON ((g).rating < 9999)
+WHERE a.tran_id = orestar.raw_committee_transactions.tran_id;
