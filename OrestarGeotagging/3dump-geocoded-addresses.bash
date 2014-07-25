229a1dce543fc60161dfa97cf8027f3a11a6f57c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-07-25 10:26:13 -0700

    dump script

---
+++ OrestarGeotagging/3dump-geocoded-addresses.bash
@@ -0,0 +1,29 @@
+#! /bin/bash
+#
+# Copyright (C) 2014 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
+export OUT=/gisdata # where the cleaned files live
+sudo mkdir -p ${OUT}
+sudo chown -R ${USER}:${USER} ${OUT}
+
+# dump the schema
+pg_dump \
+  --jobs 8 \
+  --format directory \
+  --section pre-data \
+  --section data \
+  --section post-data \
+  --encoding UTF8 \
+  --verbose \
+  --file "/gisdata/pgdump/orestar.backup" \
+  --schema "orestar" "us_geocoder"
+
+# dump the whole database
+../PostGIS/dump-database.bash us_geocoder
