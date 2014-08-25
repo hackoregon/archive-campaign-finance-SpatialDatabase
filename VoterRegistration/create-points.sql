f963a20c5b53b2a173c980c0bb3f77ccb3e04d2a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-08-25 14:07:55 -0700

    add precinct_name

--- VoterRegistration/create-points.sql
+++ VoterRegistration/create-points.sql
@@ -3,11 +3,12 @@ CREATE TABLE voter_reg.points AS
 SELECT DISTINCT
   upper(trim(both ' ' from county)) AS county,
   upper(trim(both ' ' from precinct)) AS precinct,
+  upper(trim(both ' ' from precinct_name)) AS precinct_name,
   upper(trim(both ' ' from split)) AS split,
   COUNT(addid) AS addresses,
   ST_Collect(geomout) AS points
 FROM voter_reg.addresses
 WHERE rating < 37
-GROUP BY county, precinct, split
+GROUP BY county, precinct, precinct_name, split
 ORDER BY addresses DESC;
 ALTER TABLE voter_reg.points OWNER TO znmeb;
