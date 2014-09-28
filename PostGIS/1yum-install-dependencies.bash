5eadf10ddf60f8bea37199d7583839aac2070fe5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-09-27 23:54:03 -0700

    just the necessities - QGIS, PostGIS and Spatialite

--- PostGIS/1yum-install-dependencies.bash
+++ PostGIS/1yum-install-dependencies.bash
@@ -9,71 +9,14 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-sudo cp qgis.repo /etc/yum.repos.d # latest QGIS packages live here
 sudo yum update -y
 sudo yum install -y \
-  dos2unix \
-  perl-Regexp-Assemble \
-  subversion \
-  patch \
-  gdal \
-  gdal-devel \
-  gdal-doc \
-  gdal-ruby \
-  gdal-perl \
-  gdal-python \
-  dans-gdal-scripts \
-  grass \
-  grass-devel \
-  proj \
-  proj-devel \
-  proj-epsg \
-  proj-nad \
-  geos \
-  geos-devel \
-  geos-python \
-  GMT \
-  GMT-coastlines \
-  GMT-doc \
-  GMT-coastlines-full \
-  GMT-coastlines-high \
-  GMT-common \
-  GMT-devel \
-  foxtrotgps \
-  gpscorrelate \
-  gpsdrive \
-  viking \
-  zyGrib \
-  josm \
-  carto \
-  mapnik \
-  mapnik-demo \
-  mapnik-utils \
-  marble \
-  merkaartor \
-  osm-gps-map \
-  osm-gps-map-devel \
-  osm-gps-map-gobject \
+  qgis \
+  spatialite-gui \
+  spatialite-tools \
   osm2pgsql \
-  python-phyghtmap \
   pgRouting \
   postgis \
   postgis-docs \
-  postgis-utils \
-  qgis \
-  qgis-devel \
-  qgis-grass \
-  qgis-python \
-  qlandkartegt \
-  readosm \
-  readosm-devel \
-  routino \
-  spatialite-gui \
-  spatialite-tools \
-  libspatialite-devel \
-  librasterlite-devel \
-  saga \
-  saga-devel \
-  mapserver \
-  qgis-mapserver
+  postgis-utils
 sudo ./install-pagc-address-standardizer.bash
