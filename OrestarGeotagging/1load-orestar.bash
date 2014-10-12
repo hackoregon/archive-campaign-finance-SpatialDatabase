c6082e2008be8ef6d8512d17788f7a5ff2e6ea8c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-11 22:08:56 -0700

    adjust to new dataset

--- OrestarGeotagging/1load-orestar.bash
+++ OrestarGeotagging/1load-orestar.bash
@@ -9,22 +9,36 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-# create an 'orestar' schema in the 'us_geocoder' database
+# cleanup first
 for i in \
-  "DROP SCHEMA IF EXISTS orestar CASCADE;" \
-  "CREATE SCHEMA orestar AUTHORIZATION ${USER};"
+  ac_grass_roots_in_state \
+  campaign_detail \
+  candidate_by_state \
+  cc_grass_roots_in_state \
+  cc_working_transactions \
+  direction_codes \
+  raw_candidate_filings \
+  raw_committee_transactions \
+  raw_committee_transactions_ammended_transactions \
+  raw_committees \
+  raw_committees_scraped \
+  state_translation \
+  working_candidate_committees \
+  working_candidate_filings \
+  working_committees \
+  working_transactions
 do
-  sudo su - postgres -c "psql -d us_geocoder -c '${i}'"
+  sudo su - postgres -c "psql -d us_geocoder -c \"DROP TABLE IF EXISTS ${i} CASCADE;\""
 done
+sudo su - postgres -c "psql -d us_geocoder -c \"DROP SCHEMA IF EXISTS http CASCADE;\""
 
 # restore the ORESTAR files - they go into 'public' schema by default
 sudo su - postgres -c "bzip2 -dc ${1} | psql -d us_geocoder"
 
-# move the tables to the orestar schema
+# index tables to be geocoded
 for i in \
   "CREATE INDEX ON raw_committees (treasurer_mailing_address);" \
-  "ALTER TABLE raw_committees OWNER TO ${USER};" \
-  "ALTER TABLE raw_committees SET SCHEMA orestar;" \
+  "VACUUM ANALYZE raw_committees;" \
   "CREATE INDEX ON raw_committee_transactions (addr_line1);" \
   "CREATE INDEX ON raw_committee_transactions (city);" \
   "CREATE INDEX ON raw_committee_transactions (state);" \
@@ -35,10 +49,8 @@ for i in \
   "ALTER TABLE raw_committee_transactions ADD COLUMN lon double precision;" \
   "ALTER TABLE raw_committee_transactions ADD COLUMN lat double precision;" \
   "ALTER TABLE raw_committee_transactions ADD COLUMN srid text;" \
-  "ALTER TABLE raw_committee_transactions ADD COLUMN tran_id serial NOT NULL PRIMARY KEY;" \
-  "ALTER TABLE raw_committee_transactions OWNER TO ${USER};" \
-  "ALTER TABLE raw_committee_transactions SET SCHEMA orestar;" \
-  "VACUUM ANALYZE orestar.raw_committee_transactions;"
+  "ALTER TABLE raw_committee_transactions ADD COLUMN ztran_id serial NOT NULL PRIMARY KEY;" \
+  "VACUUM ANALYZE raw_committee_transactions;"
 do
   sudo su - postgres -c "psql -d us_geocoder -c '${i}'"
 done
