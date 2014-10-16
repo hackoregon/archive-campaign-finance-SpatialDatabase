905009c052fc78a681c666cfa181145d65ce978d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-15 19:10:51 -0700

    new table name!

--- OrestarGeotagging/count.sql
+++ OrestarGeotagging/count.sql
@@ -1,8 +1,3 @@
 ﻿\t
 \a
-SELECT COUNT(zip) FROM raw_committee_transactions 
-WHERE rating IS NULL 
-AND addr_line1 IS NOT NULL
-AND city IS NOT NULL
-AND state IS NOT NULL
-AND zip IS NOT NULL;
+SELECT COUNT(address) FROM geocoded_transactions WHERE rating IS NULL;
