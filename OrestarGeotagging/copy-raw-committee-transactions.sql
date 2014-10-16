c37f2a774876a5d8fc1cc959dc9a77dcdb5a7ab5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-15 18:48:46 -0700

    cleanup

--- OrestarGeotagging/copy-raw-committee-transactions.sql
+++ OrestarGeotagging/copy-raw-committee-transactions.sql
@@ -1,22 +1,20 @@
 DROP TABLE IF EXISTS geocoded_transactions;
 CREATE TABLE geocoded_transactions TABLESPACE spatial AS 
-  SELECT * FROM raw_committee_transactions 
-    WHERE addr_line1 IS NOT NULL
-    AND addr_line1 !~ '\*'
-    AND addr_line1 !~ '\?'
-    AND city IS NOT NULL
-    AND state IS NOT NULL
-    AND zip IS NOT NULL
+  SELECT tran_id,
+    concat_ws(' ', addr_line1, COALESCE(addr_line2, ' '), city, state, zip) 
+    AS address
+  FROM raw_committee_transactions 
+  WHERE addr_line1 IS NOT NULL
+  AND addr_line1 !~ '\*'
+  AND addr_line1 !~ '\?'
+  AND city IS NOT NULL
+  AND state IS NOT NULL
+  AND zip IS NOT NULL
 ;
-CREATE INDEX ON geocoded_transactions (addr_line1);
-CREATE INDEX ON geocoded_transactions (city);
-CREATE INDEX ON geocoded_transactions (state);
-CREATE INDEX ON geocoded_transactions (zip);
-ALTER TABLE geocoded_transactions ADD COLUMN ztran_id serial NOT NULL PRIMARY KEY;
-ALTER TABLE geocoded_transactions ADD COLUMN addy norm_addy;
+CREATE INDEX ON geocoded_transactions (address);
+ALTER TABLE geocoded_transactions
+  ADD COLUMN ztran_id serial NOT NULL PRIMARY KEY;
 ALTER TABLE geocoded_transactions ADD COLUMN geomout geometry;
 ALTER TABLE geocoded_transactions ADD COLUMN rating integer;
-ALTER TABLE geocoded_transactions ADD COLUMN lon double precision;
-ALTER TABLE geocoded_transactions ADD COLUMN lat double precision;
-ALTER TABLE geocoded_transactions ADD COLUMN srid text;
+ALTER TABLE geocoded_transactions ADD COLUMN srid integer;
 VACUUM ANALYZE geocoded_transactions;
