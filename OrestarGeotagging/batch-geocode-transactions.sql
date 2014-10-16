e677baaf4d327c003ac94daddf7980139d152d0c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-15 19:40:47 -0700

    2500 at a time

--- OrestarGeotagging/batch-geocode-transactions.sql
+++ OrestarGeotagging/batch-geocode-transactions.sql
@@ -5,7 +5,7 @@ SET
   COALESCE((g).rating, 9999),
   4269
 )
-FROM (SELECT * FROM geocoded_transactions WHERE rating IS NULL LIMIT 1000) AS a
+FROM (SELECT * FROM geocoded_transactions WHERE rating IS NULL LIMIT 2500) AS a
 LEFT JOIN LATERAL geocode(a.address, 1) AS g
 ON ((g).rating < 9999)
 WHERE a.ztran_id = geocoded_transactions.ztran_id;
