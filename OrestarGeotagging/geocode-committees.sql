6be9d868c045cd133a98d62f03e810bf5316d29b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-17 11:32:04 -0700

    GeoJSON

--- OrestarGeotagging/geocode-committees.sql
+++ OrestarGeotagging/geocode-committees.sql
@@ -1,12 +1,7 @@
 UPDATE geocoded_committees
 SET
-(geomout, rating, srid) = (
-  (g).geomout,
-  COALESCE((g).rating, 9999),
-  4269  
-)
+(geojson, rating) = (ST_AsGeoJSON((g).geomout), COALESCE((g).rating, 9999))
 FROM (SELECT * FROM geocoded_committees WHERE rating IS NULL) AS a
-LEFT JOIN LATERAL
-  geocode(a.treasurer_mailing_address, 1) AS g
+LEFT JOIN LATERAL geocode(a.treasurer_mailing_address, 1) AS g
 ON ((g).rating < 9999)
 WHERE a.zcommittee_id = geocoded_committees.zcommittee_id;
