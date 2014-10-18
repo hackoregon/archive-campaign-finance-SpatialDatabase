2ce29292b401f5cd4d748dfca07cfa0b0a38c1b7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-18 00:28:39 -0700

    just GeoJSON and rating

--- VoterRegistration/batch-geocode.sql
+++ VoterRegistration/batch-geocode.sql
@@ -1,15 +1,11 @@
 UPDATE voter_reg.addresses
 SET
-(addy, geomout, rating, lon, lat, srid) = (
-  (g).addy,
-  (g).geomout,
-  COALESCE((g).rating, 9999),
-  ST_X((g).geomout),
-  ST_Y((g).geomout),
-  '4269'
-)
-FROM (SELECT * FROM voter_reg.addresses
-WHERE rating IS NULL LIMIT 10000) AS a
+(geojson, rating) = (ST_As_GeoJSON((g).geomout), COALESCE((g).rating, 9999))
+FROM (
+  SELECT * FROM voter_reg.addresses
+  WHERE rating IS NULL
+  LIMIT 10000
+) AS a
 LEFT JOIN LATERAL geocode(a.address, 1) AS g
 ON ((g).rating < 9999)
 WHERE a.addid = voter_reg.addresses.addid;
