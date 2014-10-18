9ec6322e64448c0e3983612dfc30b1a32438cec2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-10-18 00:31:23 -0700

    clones for parallel run

---
+++ VoterRegistration/bg3.sql
@@ -0,0 +1,12 @@
+UPDATE voter_reg.addresses
+SET
+(geojson, rating) = (ST_As_GeoJSON((g).geomout), COALESCE((g).rating, 9999))
+FROM (
+  SELECT * FROM voter_reg.addresses
+  WHERE rating IS NULL
+  AND addid % 6 = 3
+  LIMIT 10000
+) AS a
+LEFT JOIN LATERAL geocode(a.address, 1) AS g
+ON ((g).rating < 9999)
+WHERE a.addid = voter_reg.addresses.addid;
